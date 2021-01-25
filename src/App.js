8d60f727185a72d6af187b168e6709de97eca523
ankur <ciscojorwal@gmail.com>
2021-01-25 13:19:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import QuesContainer from "./QuesContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <QuesContainer />
+    </div>
+  );
+}
+
+export default App;
